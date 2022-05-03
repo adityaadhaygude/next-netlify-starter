ff26f21773788dbbce2d0b1d3508ce5bed149e21
Aditya Dhaygude <40889126+adityaadhaygude@users.noreply.github.com>
2022-05-03 13:33:13 +0530

    Update Footer.js
    added script in footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
+      
+</footer><!--Begin: Star-Clicks.com HTML Code--><script type='text/javascript' src='https://www.star-clicks.com/secure/ads.php?pid=48072112052330443'></script><!-- End: Star-Clicks.com -->
     </>
   )
 }
